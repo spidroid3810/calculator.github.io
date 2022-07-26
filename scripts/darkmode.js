0eab56016a33502c6d83dcde356a694f283dac17
Spidroid381 <102158589+SPIDROID381@users.noreply.github.com>
2022-07-26 23:21:50 +0530

    Delete darkmode.js

--- scripts/darkmode.js
+++
@@ -1,8 +0,0 @@
-const checkbox = document.getElementById('checkbox');
-		
-		checkbox.addEventListener('change', ()=>{
-		document.body.classList.toggle('dark');
-		})
-		
-		function play() {var audio = document.getElementById("audio"); audio.play();
-		}
