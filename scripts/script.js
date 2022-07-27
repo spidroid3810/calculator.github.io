685072513c880a6391869fcbee8ff9089e427680
Spidroid381 <102158589+SPIDROID381@users.noreply.github.com>
2022-07-27 09:19:41 +0530

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -33,16 +33,16 @@ function themeSwitcher() {
   "theme");
 
  if (th.getAttribute('href') ==
-  'scripts/light.css') {
+  'styles/light.css') {
 
-  th.href = 'scripts/dark.css';
+  th.href = 'styles/dark.css';
 
   dark.innerHTML =
    '<i class="bi-brightness-high-fill"></i>';
 
  } else {
 
-  th.href = 'scripts/light.css';
+  th.href = 'styles/light.css';
 
   dark.innerHTML =
    '<i class="fa fa-moon-o"></i>';
