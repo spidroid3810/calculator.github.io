1bb0a0b5f24f48a825715a1595595559708b4020
Spidroid381 <102158589+SPIDROID381@users.noreply.github.com>
2022-07-30 10:36:20 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -66,3 +66,15 @@ const pages = document.querySelectorAll(".page");
  
  function play() {var audio = document.getElementById("audio"); audio.play();
  }
+ 
+ 
+ var myVar;
+ 
+ function myFunction() {
+ myVar = setTimeout(showPage, 3000);
+ }
+ 
+ function showPage() {
+ document.getElementById("loader").style.display = "none";
+ document.getElementById("myDiv").style.display = "block";
+ }
