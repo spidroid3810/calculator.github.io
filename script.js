21bc49bd6f313b6f406de5ca3c559b2ac7a22a15
Spidroid381 <102158589+SPIDROID381@users.noreply.github.com>
2022-08-01 21:38:14 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ const pages = document.querySelectorAll(".page");
  th.href = 'dark.css';
  
  dark.innerHTML =
- '<i class="fa fa-moon-o"></i>';
+ 'dark mode';
  
  } else {
  
@@ -33,7 +33,7 @@ const pages = document.querySelectorAll(".page");
  
  dark.innerHTML =
  
- '<i class="bi-brightness-high-fill"></i>';
+ 'light mode';
  
  }
  
@@ -63,9 +63,9 @@ const pages = document.querySelectorAll(".page");
  .value = x + val;
  
  }
- 
- 
- 
+  
+
+  
  function play() {var audio = document.getElementById("audio"); audio.play();
  }
  
@@ -82,58 +82,7 @@ const pages = document.querySelectorAll(".page");
  }
  
 
-function getUserPreference() {
-  return localStorage.getItem('theme') || 'system';
-}
-function saveUserPreference(userPreference) {
-  localStorage.setItem('theme', userPreference);
-}
 
-function getAppliedMode(userPreference) {
-  if (userPreference === 'light') {
-    return 'light';
-  }
-  if (userPreference === 'dark') {
-    return 'dark';
-  }
-  // system
-  if (matchMedia('(prefers-color-scheme: light)').matches) {
-    return 'light';
-  }
-  return 'dark';
-}
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
 
-const colorScheme = document.querySelector('meta[name="color-scheme"]');
-function setAppliedMode(mode) {
-  document.body.className = mode;
-  colorScheme.content = mode;
-}
-
-function rotatePreferences(userPreference) {
-  if (userPreference === 'system') {
-    return 'light'
-  }
-  if (userPreference === 'light') {
-    return 'dark';
-  }
-  if (userPreference === 'dark') {
-    return 'system';
-  }
-  // for invalid values, just in case
-  return 'system';
-}
-
-const themeDisplay = document.getElementById('mode');
-const themeToggler = document.getElementById('theme-toggle');
-
-let userPreference = getUserPreference();
-setAppliedMode(getAppliedMode(userPreference));
-themeDisplay.innerText = userPreference;
-
-themeToggler.onclick = () => {
-  const newUserPref = rotatePreferences(userPreference);
-  userPreference = newUserPref;
-  saveUserPreference(newUserPref);
-  themeDisplay.innerText = newUserPref;
-  setAppliedMode(getAppliedMode(newUserPref));
-}
+    
